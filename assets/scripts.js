4f62d2dd2bd7f3c58708f599feb08b9428f99dab
James Lovejoy <james@sparkart.com>
2013-07-22 23:18:37 -0700

    Allow more growth rates for projection.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,36 +1,65 @@
 $(document).ready(function() {  
-  var states = [];
-  var seats = {};
+  var $dataSource = $("select.source");
+  var $tBody = $("tbody");
 
   // Function to get the Max value in Array
   Array.max = function( array ){
       return Math.max.apply( Math, array );
   };
 
-  $.getJSON('data/population.json', function(data) {
-    $.each(data, function(index, state) {
-      var items = [];
-      var state_name = state.state;
-      var pop2010 = state.pop2010;
-      var percentage = Math.round(state.change*10000)/100;
-      var pop2020 = Math.round(pop2010 * (percentage / 100)) + parseInt(pop2010);
+  var runProjection = function(){
+    var states = [];
+    var seats = {};
+    var dataSource = $dataSource.find("option:selected").val();
 
-      states.push(state_name);
+    $.getJSON(dataSource, function(data) {
+      $tBody.html('');
 
-      items.push("<td class=\"state\">"+ state_name +"</td>");
-      items.push("<td class=\"current-population\">"+ pop2010 +"</td>");
-      items.push("<td class=\"projected-population\">"+ pop2020 +"</td>");
-      items.push("<td data-value=\""+ percentage +"\">"+ percentage +"%</td>");
-      items.push("<td class=\"current-apportionment\"></td>");
-      items.push("<td class=\"projected-apportionment\"></td>");
-      items.push("<td class=\"difference\"></td>");
- 
-      $('<tr/>', {
-        'class': 'state',
-        html: items.join('')
-      }).appendTo('tbody');
+      $.each(data, function(index, state) {
+        var items = [];
+        var state_name = state.state;
+        var pop2010 = state.pop2010;
+        var percentage = Math.round(state.change*10000)/100;
+        var pop2020 = Math.round(pop2010 * (percentage / 100)) + parseInt(pop2010);
+
+        states.push(state_name);
+
+        items.push('<td class="state">'+ state_name +'</td>');
+        items.push('<td class="current-population">'+ pop2010 +'</td>');
+        items.push('<td class="projected-population">'+ pop2020 +'</td>');
+        items.push('<td data-value="'+ percentage +'">'+ percentage +'%</td>');
+        items.push('<td class="current-apportionment"></td>');
+        items.push('<td class="projected-apportionment"></td>');
+        items.push('<td class="difference"></td>');
+   
+        $('<tr/>', {
+          'class': 'state',
+          html: items.join('')
+        }).appendTo('tbody');
+      });
+    }).done(function(){
+
+      runApportionment($("td.current-population"), $("td.current-apportionment"));
+      runApportionment($("td.projected-population"), $("td.projected-apportionment"));
+
+      jQuery.each( $("tbody tr"), function(index, tr) {
+        var $tr =  $(tr);
+        var current = $tr.find("td.current-apportionment").text();
+        var projected = $tr.find("td.projected-apportionment").text();
+
+        var difference = projected - current;
+        var sign = (difference > 0 ? "+" : "");
+        var klass = (difference > 1) ? "gain-more" : (difference == 1 ? "gain" : (difference == -1 ? "lose" : (difference < -1 ? "lose-more" : "")));
+
+        $tr.addClass(klass);
+        $tr.find("td.difference").addClass(klass).text( sign + difference );
+      });
+
+      $("table").addClass("sortable");
+      $.bootstrapSortable();
+      $tBody.css({opacity: 1});
+
     });
-  }).done(function(){
 
     runApportionment = function(population_column, output_column) {
       var population = jQuery.map( population_column, function(td) { return $(td).text(); } );
@@ -41,52 +70,41 @@ $(document).ready(function() {
       });
     }
 
-    runApportionment($("td.current-population"), $("td.current-apportionment"));
-    runApportionment($("td.projected-population"), $("td.projected-apportionment"));
+    findHighestPriorityState = function(population) {
+      var priorities = jQuery.map( states, function(state, index) {
+        var pop = population[index];
+        var n =  seats[state];
+        var priority = pop / Math.sqrt(n*(n+1));
+        
+        return priority;
+      });
 
-    jQuery.each( $("tbody tr"), function(index, tr) {
-      var $tr =  $(tr);
-      var current = $tr.find("td.current-apportionment").text();
-      var projected = $tr.find("td.projected-apportionment").text();
+      var index = priorities.indexOf( Array.max(priorities) );
+      return states[index];
+    };
 
-      var difference = projected - current;
-      var sign = (difference > 0 ? "+" : "");
-      var klass = (difference > 1) ? "gain-more" : (difference == 1 ? "gain" : (difference == -1 ? "lose" : (difference < -1 ? "lose-more" : "")));
+    apportion = function(population) {
+      // Assign 1 seat to each state
+      jQuery.each( states, function(index, state) {
+        seats[state] = 1;
+      });
 
-      $tr.addClass(klass);
-      $tr.find("td.difference").addClass(klass).text( sign + difference );
-    });
+      for(n = 0; n < 385; n++) {
+        highest = findHighestPriorityState(population);
+        seats[highest] += 1;
+      }
 
-    $("table").addClass("sortable");
-    $.bootstrapSortable();
+      return seats;
+    };
 
-  });
-
-  findHighestPriorityState = function(population) {
-    var priorities = jQuery.map( states, function(state, index) {
-      var pop = population[index];
-      var n =  seats[state];
-      var priority = pop / Math.sqrt(n*(n+1));
-      
-      return priority;
-    });
-
-    var index = priorities.indexOf( Array.max(priorities) );
-    return states[index];
   };
 
-  apportion = function(population) {
-    // Assign 1 seat to each state
-    jQuery.each( states, function(index, state) {
-      seats[state] = 1;
-    });
+  $dataSource.on("change", function(){
+    $tBody.css({opacity: 0.5});
 
-    for(n = 0; n < 385; n++) {
-      highest = findHighestPriorityState(population);
-      seats[highest] += 1;
-    }
+    runProjection();
+  });
 
-    return seats;
-  };  
+  runProjection();
 
 });
